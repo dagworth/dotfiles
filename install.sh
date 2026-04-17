7504c6a33a28c9743a6d3c3cf55eefedbd7354f6
larry <larryhe360@gmail.com>
2026-04-17 20:35:11 +0000

    init

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DOTS="$HOME/dotfiles"
+CONFIG="$HOME/.config"
+
+apps=("hypr" "kitty" "waybar" "tofi")
+
+for app in "${apps[@]}"; do
+    if [ -d "$HOME/dotfiles/$app" ]; then
+        ln -sfn "$HOME/dotfiles/$app" "$HOME/.config/$app"
+    else
+        echo "$DOTS/$app doesnt exist"
+    fi
+done
+
+PROFILE_NAME=$(ls "$HOME/.config/mozilla/firefox" | grep default | head -n 1)
+FF_PROFILE_PATH="$HOME/.config/mozilla/firefox/$PROFILE_NAME"
+
+if [ -n "$PROFILE_NAME" ]; then
+    mkdir -p "$FF_PROFILE_PATH/chrome"
+    ln -sfn "$HOME/dotfiles/firefox/chrome/userChrome.css" "$FF_PROFILE_PATH/chrome/userChrome.css"
+    ln -sfn "$HOME/dotfiles/firefox/chrome/userContent.css" "$FF_PROFILE_PATH/chrome/userContent.css"
+else
+    echo "couldnt find firefox profile"
+fi
